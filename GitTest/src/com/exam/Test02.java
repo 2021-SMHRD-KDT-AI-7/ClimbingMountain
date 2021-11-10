c8397032e020c97e4a41bee44790eb8d82f11a4b
smhrd <smhrd@220.80.88.59>
2021-11-10 16:31:27 +0900

    정찬준 오후 4시 30분

--- GitTest/src/com/exam/Test02.java
+++ GitTest/src/com/exam/Test02.java
@@ -3,15 +3,12 @@ package com.exam;
 public class Test02 {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("¼­ ¹øÂ° Ä¿¹Ô!");
 		System.out.println("³× ¹øÂ° Ä¿¹Ô!");
 		
 			
-=======
 		System.out.println("°û½Â¿Á ¿ÀÈÄ 4½Ã 25ºĞ Ä¿¹Ô!");
-
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-KDT-AI-7/ClimbingMountain.git
+		System.out.println("ÂùÁØ¿ÀÈÄ 4½Ã 30ºĞ");
 	}
 
 }
