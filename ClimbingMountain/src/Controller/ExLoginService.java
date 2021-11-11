f772022ae229094bf2f6a584d492981e4eeea65f
smhrd <smhrd@211.227.224.206>
2021-11-11 16:26:47 +0900

    1차프로젝트

---
+++ ClimbingMountain/src/Controller/ExLoginService.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ExLoginService")
+public class ExLoginService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public ExLoginService() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
