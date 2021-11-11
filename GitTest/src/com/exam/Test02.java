09e20399c12ea7b27f384e5964be2e9155ba31ce
smhrd <smhrd@211.227.224.206>
2021-11-11 13:58:44 +0900

    깃허브 사용법 연습

--- GitTest/src/com/exam/Test02.java
+++ GitTest/src/com/exam/Test02.java
@@ -4,11 +4,12 @@ public class Test02 {
 
 	public static void main(String[] args) {		System.out.println("서 번째 커밋!");
 		System.out.println("네 번째 커밋!");
-		
-			
+					
 		System.out.println("곽승옥 오후 4시 25분 커밋!");
-		System.out.println("찬준오후 4시 30분");
+		System.out.println("찬준 오후 4시 30분");
 		System.out.println("혜준 오후4시 34분 커밋!");
+		
+		System.out.println("깃허브 사용법을 익혀놓자");
 	}
 
 }
