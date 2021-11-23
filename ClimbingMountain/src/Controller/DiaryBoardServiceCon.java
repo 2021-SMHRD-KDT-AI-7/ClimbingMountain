dc334f7985f8df1280f82917b5a73d3f58c74f12
smhrd <smhrd@211.227.224.206>
2021-11-23 16:06:59 +0900

    깔끔하게 기본기능 구현된 커밋본_추후 수정가능_태욱

--- ClimbingMountain/src/Controller/DiaryBoardServiceCon.java
+++ ClimbingMountain/src/Controller/DiaryBoardServiceCon.java
@@ -1,29 +1,29 @@
-package Controller;
-
-import java.io.IOException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.Date;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.oreilly.servlet.MultipartRequest;
-import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
-
-import Interface.Command;
-import Model.DiaryDAO;
-import Model.DiaryDTO;
-import Model.MemberDTO;
-import Model.communityDAO;
-import Model.communityDTO;
-
-public class DiaryBoardServiceCon implements Command {
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		return null;
-	}
-
-}
+//package Controller;
+//
+//import java.io.IOException;
+//import java.net.URLEncoder;
+//import java.util.ArrayList;
+//import java.util.Date;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//
+//import com.oreilly.servlet.MultipartRequest;
+//import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+//
+//import Interface.Command;
+//import Model.DiaryDAO;
+//import Model.DiaryDTO;
+//import Model.MemberDTO;
+//import Model.communityDAO;
+//import Model.communityDTO;
+//
+//public class DiaryBoardServiceCon implements Command {
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//	
+//		return null;
+//	}
+//
+//}
